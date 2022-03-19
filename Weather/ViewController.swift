445cf1e72c0725a07568ca7bb725c1b2dfb2dca5
eunyupp kim <kimeunyupp@gmail.com>
2022-03-19 23:13:13 +0900

    도시이름입력해서 api를 통해 날씨정보 가져오기

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var weatherDescriptionLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var maxTempLabel: UILabel!
+    @IBOutlet weak var minTempLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func tapFetchWeatherButton(_ sender: UIButton) {
+        if let cityName = self.cityNameTextField.text {
+            self.getCurrentWeather(cityName: cityName)
+            self.view.endEditing(true)
+        }
+    }
+    
+    func getCurrentWeather(cityName: String) {
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=73d275c39182f0aa8e54aeeb3ca945cf") else {
+            return }
+        let session = URLSession(configuration: .default)
+        session.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            let decoder = JSONDecoder()
+            let weatherInformation = try?decoder.decode(WeatherInformation.self, from: data)
+            debugPrint(weatherInformation)
+        }.resume()
+        
+        
+    }
 }
 
